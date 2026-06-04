fe8bb7a8a9e6585b4396fd05dc04735bdbd11d42
SAIEED12 <abusaieed0@gmail.com>
2026-06-04 10:10:32 +0600

    refactor job API endpoints to include query parameters for filtering

--- index.js
+++ index.js
@@ -33,8 +33,22 @@ async function run() {
      
     const database = client.db("hireloop_db")
     const jobCollection = database.collection("jobs")
+    const companyCollection = database.collection("companies")
 
-    app.post('/jobs', async(req, res) =>{
+    app.get('/api/jobs', async(req, res) =>{
+      const query = {}
+      if(req.query.companyId){
+        query.companyId = req.query.companyId
+      }
+      if(req.query.status){
+        query.status = req.query.status
+      }
+      const cursor = jobCollection.find(query)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.post('/api/jobs', async(req, res) =>{
         const job = req.body
         const result = await jobCollection.insertOne(job)
         res.send(result)
